66b5cf4427911cf676a27e57d270996b16a36fc5
Austin Ameh <arewageek@gmail.com>
2025-04-25 00:25:56 +0100

    Add StarRepo button component and integrate into layout

---
+++ client/src/components/buttons/star-repo.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import GitHubButton from 'react-github-btn'
+
+const StarRepo = () => {
+    return (
+        <GitHubButton href="https://github.com/arewageek/xonpad" data-color-scheme="no-preference: dark; light: dark; dark: dark;" data-size="large" data-show-count="true" aria-label="Star arewageek/ladybug-2 on GitHub">
+            Star this repo
+        </GitHubButton>
+    )
+}
+
+export default StarRepo
